8dffec7759b89e4bfc2a2cc3be266e2d3e551d12
HarishDuck <141107791+HarishDuck@users.noreply.github.com>
2023-09-24 08:57:16 +0530

    Testing form3b parser

--- pdf-parser/core.py
+++ pdf-parser/core.py
@@ -4,4 +4,5 @@ from extractText import *
 # for now this will only support single file will change it for multiple files
 filename = fileUpload()
 
+# for now this wont work cause i am now testing form3b parser
 resultArray = extractText(filename)
--- pdf-parser/extractText.py
+++
@@ -1,9 +0,0 @@
-import pdfquery
-import pandas as pd
-
-def extractText(file):
-    pdf = pdfquery.PDFQuery(file)
-    pdf.load()
-    pdf.tree.write('text.txt', pretty_print=True)
-    year = pdf.pq('LTTextLineHorizontal:overlaps_bbox("1058, 765, 1082, 776")').text()
-    print(year)
